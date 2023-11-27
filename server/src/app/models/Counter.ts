3e352d9b6d341b6dff65ed433204ca54c56e81dc
NekoCyan <cyanhijirikawa88@gmail.com>
2023-11-27 14:42:38 +0700

    feat: use method of schema instead of import to static for statics and methods.

--- server/src/app/models/Counter.ts
+++ server/src/app/models/Counter.ts
@@ -22,19 +22,20 @@ const CounterSchema = new mongoose.Schema<
 });
 
 // statics.
-CounterSchema.statics.getNextSequence = async function (
-	modelName: IModels | string,
-	fieldName: string,
-) {
-	if (typeof modelName === 'function') modelName = modelName.modelName;
+CounterSchema.static(
+	'getNextSequence',
+	async function (modelName: IModels | string, fieldName: string): Promise<number> {
+		if (typeof modelName === 'function') modelName = modelName.modelName;
 
-	const counter = await this.findOneAndUpdate(
-		{ _id: `${modelName}.${fieldName}` },
-		{ $inc: { seq: 1 } },
-		{ new: true, upsert: true },
-	);
-	return counter.seq;
-};
+		const counter: ICounter = await this.findOneAndUpdate(
+			{ _id: `${modelName}.${fieldName}` },
+			{ $inc: { seq: 1 } },
+			{ new: true, upsert: true },
+		);
+
+		return counter.seq;
+	},
+);
 
 const CounterModel =
 	// @ts-ignore
--- server/src/app/models/User.ts
+++ server/src/app/models/User.ts
@@ -64,182 +64,202 @@ const UserSchema = new mongoose.Schema<IUser, IUserModel, IUserMethods>({
 });
 
 // validation.
-UserSchema.path('email').validate(async function (value: any) {
+UserSchema.path('email').validate(async function (
+	value: any,
+): Promise<boolean> {
 	if (!this.isNew) return true;
 	return (await this.model('User').countDocuments({ email: value })) === 0;
-}, ResponseText.AlreadyExists('email'));
+},
+ResponseText.AlreadyExists('email'));
 
 // methods.
-UserSchema.methods.comparePassword = async function (
-	password: string,
-): Promise<boolean> {
-	return await Password_Compare(password, this.password);
-};
-UserSchema.methods.generateAuthToken = async function (
-	expiresIn: number = 24 * 60 * 60,
-): Promise<string> {
-	return await JWT_Sign(
-		{
-			userId: this.userId,
-		},
-		expiresIn,
-	);
-};
+UserSchema.method(
+	'comparePassword',
+	async function (password: string): Promise<boolean> {
+		return await Password_Compare(password, this.password);
+	},
+);
+UserSchema.method(
+	'generateAuthToken',
+	async function (expiresIn: number = 24 * 60 * 60): Promise<string> {
+		return await JWT_Sign(
+			{
+				userId: this.userId,
+			},
+			expiresIn,
+		);
+	},
+);
 
 // statics.
-UserSchema.statics.findByCredentials = async (
-	email: string,
-	password: string,
-): Promise<UserHydratedDocument> => {
-	if (!email) throw new Error(ResponseText.Required('email'));
-	if (!password) throw new Error(ResponseText.Required('password'));
+UserSchema.static(
+	'findByCredentials',
+	async function (
+		email: string,
+		password: string,
+	): Promise<UserHydratedDocument> {
+		if (!email) throw new Error(ResponseText.Required('email'));
+		if (!password) throw new Error(ResponseText.Required('password'));
 
-	const user = await UserModel.findOne({ email });
-	if (!user) throw new Error(ResponseText.NotExists('email'));
+		const user = await UserModel.findOne({ email });
+		if (!user) throw new Error(ResponseText.NotExists('email'));
 
-	const isPasswordMatch = await user.comparePassword(password);
-	if (!isPasswordMatch) throw new Error(ResponseText.NotMatch('password'));
+		const isPasswordMatch = await user.comparePassword(password);
+		if (!isPasswordMatch)
+			throw new Error(ResponseText.NotMatch('password'));
 
-	return user;
-};
-UserSchema.statics.findByAuthToken = async function (
-	token: string,
-): Promise<UserHydratedDocument> {
-	const verify = await this.decodeAuthToken(token);
+		return user;
+	},
+);
+UserSchema.static(
+	'findByAuthToken',
+	async function (token: string): Promise<UserHydratedDocument> {
+		const verify = await this.decodeAuthToken(token);
 
-	const user = await UserModel.findOne({
-		userId: verify.userId,
-	});
-	if (!user) throw new Error(ResponseText.Unauthorized);
+		const user = await UserModel.findOne({
+			userId: verify.userId,
+		});
+		if (!user) throw new Error(ResponseText.Unauthorized);
 
-	return user;
-};
-UserSchema.statics.decodeAuthToken = async function (
-	token: string,
-): Promise<TokenPayload> {
-	const verify = await JWT_Verify(token);
-	if (!verify) throw new Error(ResponseText.Unauthorized);
+		return user;
+	},
+);
+UserSchema.static(
+	'decodeAuthToken',
+	async function (token: string): Promise<TokenPayload> {
+		const verify = await JWT_Verify(token);
+		if (!verify) throw new Error(ResponseText.Unauthorized);
 
-	return verify;
-};
-UserSchema.statics.extractUserInformations = async function (
-	data: Partial<UserInformations>,
-): Promise<Partial<UserInformations>> {
-	let { fullName, gender, address, phoneNumber, photo } = data;
-	let updateObj: Partial<UserInformations> = {};
+		return verify;
+	},
+);
+UserSchema.static(
+	'extractUserInformations',
+	async function (
+		data: Partial<UserInformations>,
+	): Promise<Partial<UserInformations>> {
+		let { fullName, gender, address, phoneNumber, photo } = data;
+		let updateObj: Partial<UserInformations> = {};
 
-	const validateGender = (gender: any) => {
-		if (isNaN(gender))
-			throw new Error(ResponseText.InvalidType('gender', 'number'));
-		if (typeof gender === 'string') gender = parseInt(gender) as -1 | 0 | 1;
-		if (gender < -1 || gender > 1)
-			throw new Error(ResponseText.OutOfRange('gender', -1, 1));
-		updateObj.gender = gender;
-	};
-	const validateFullName = (fullName: any) => {
-		if (typeof fullName !== 'string')
-			throw new Error(ResponseText.InvalidType('fullName', 'string'));
-		updateObj.fullName = fullName;
-	};
-	const validateAddress = (address: any) => {
-		if (typeof address !== 'string')
-			throw new Error(ResponseText.InvalidType('address', 'string'));
-		updateObj.address = address;
-	};
-	const validatePhoneNumber = (phoneNumber: any) => {
-		if (typeof phoneNumber !== 'string')
-			throw new Error(ResponseText.InvalidType('phoneNumber', 'string'));
-		updateObj.phoneNumber = phoneNumber;
-	};
-	const validatePhoto = (photo: any) => {
-		if (typeof photo !== 'string')
-			throw new Error(ResponseText.InvalidType('photo', 'string'));
-		updateObj.photo = photo;
-	};
+		const validateGender = (gender: any) => {
+			if (isNaN(gender))
+				throw new Error(ResponseText.InvalidType('gender', 'number'));
+			if (typeof gender === 'string') gender = parseInt(gender);
+			if (gender < -1 || gender > 1)
+				throw new Error(ResponseText.OutOfRange('gender', -1, 1));
+			updateObj.gender = gender;
+		};
+		const validateFullName = (fullName: any) => {
+			if (typeof fullName !== 'string')
+				throw new Error(ResponseText.InvalidType('fullName', 'string'));
+			updateObj.fullName = fullName;
+		};
+		const validateAddress = (address: any) => {
+			if (typeof address !== 'string')
+				throw new Error(ResponseText.InvalidType('address', 'string'));
+			updateObj.address = address;
+		};
+		const validatePhoneNumber = (phoneNumber: any) => {
+			if (typeof phoneNumber !== 'string')
+				throw new Error(
+					ResponseText.InvalidType('phoneNumber', 'string'),
+				);
+			updateObj.phoneNumber = phoneNumber;
+		};
+		const validatePhoto = (photo: any) => {
+			if (typeof photo !== 'string')
+				throw new Error(ResponseText.InvalidType('photo', 'string'));
+			updateObj.photo = photo;
+		};
 
-	!IsUndefined(gender) && validateGender(gender);
-	!IsUndefined(fullName) && validateFullName(fullName);
-	!IsUndefined(address) && validateAddress(address);
-	!IsUndefined(phoneNumber) && validatePhoneNumber(phoneNumber);
-	!IsUndefined(photo) && validatePhoto(photo);
+		!IsUndefined(gender) && validateGender(gender);
+		!IsUndefined(fullName) && validateFullName(fullName);
+		!IsUndefined(address) && validateAddress(address);
+		!IsUndefined(phoneNumber) && validatePhoneNumber(phoneNumber);
+		!IsUndefined(photo) && validatePhoto(photo);
 
-	return updateObj;
-};
-UserSchema.statics.extractUserData = async function (
-	data: Partial<UserData>,
-): Promise<Partial<UserData>> {
-	let { password, role } = data;
-	let updateObj: Partial<UserData> = {};
+		return updateObj;
+	},
+);
 
-	const validatePassword = (password: any) => {
-		if (typeof password !== 'string')
-			throw new Error(ResponseText.InvalidType('password', 'string'));
-		updateObj.password = password;
-	};
-	const validateRole = (role: any) => {
-		if (isNaN(role))
-			throw new Error(ResponseText.InvalidType('role', 'number'));
-		if (typeof role === 'string') role = parseInt(role) as 0 | 1 | 2;
-		if (role < 0 || role > 2)
-			throw new Error(ResponseText.OutOfRange('role', 0, 2));
-		updateObj.role = role;
-	};
+UserSchema.static(
+	'extractUserData',
+	async function (data: Partial<UserData>): Promise<Partial<UserData>> {
+		let { password, role } = data;
+		let updateObj: Partial<UserData> = {};
 
-	!IsUndefined(password) && validatePassword(password);
-	!IsUndefined(role) && validateRole(role);
+		const validatePassword = (password: any) => {
+			if (typeof password !== 'string')
+				throw new Error(ResponseText.InvalidType('password', 'string'));
+			updateObj.password = password;
+		};
+		const validateRole = (role: any) => {
+			if (isNaN(role))
+				throw new Error(ResponseText.InvalidType('role', 'number'));
+			if (typeof role === 'string') role = parseInt(role);
+			if (role < 0 || role > 2)
+				throw new Error(ResponseText.OutOfRange('role', 0, 2));
+			updateObj.role = role;
+		};
 
-	return updateObj;
-};
-UserSchema.statics.updateUser = async function (
-	userId: number,
-	data: Partial<UserData & UserInformations>,
-	extraData?: { [key: string]: any },
-): Promise<UserHydratedDocument> {
-	const user = await this.findOne({ userId });
-	if (!user) throw new Error(ResponseText.Unauthorized);
+		!IsUndefined(password) && validatePassword(password);
+		!IsUndefined(role) && validateRole(role);
 
-	let updateObj: Partial<UserData & UserInformations> & {
-		[key: string]: any;
-	} = {};
+		return updateObj;
+	},
+);
+UserSchema.static(
+	'updateUser',
+	async function (
+		userId: number,
+		data: Partial<UserData & UserInformations>,
+		extraData?: { [key: string]: any },
+	): Promise<UserHydratedDocument> {
+		const user = await this.findOne({ userId });
+		if (!user) throw new Error(ResponseText.Unauthorized);
 
-	// UserInformations.
-	const userInformations = (await this.extractUserInformations(data).catch(
-		(e) => e,
-	)) as Partial<UserInformations>;
-	if (userInformations instanceof Error) throw userInformations;
-	// UserData.
-	const userData = (await this.extractUserData(data).catch(
-		(e) => e,
-	)) as Partial<UserData>;
-	if (userData instanceof Error) throw userData;
+		let updateObj: Partial<UserData & UserInformations> & {
+			[key: string]: any;
+		} = {};
 
-	// Group validated fields to update.
-	updateObj = { ...userInformations, ...userData };
+		// UserInformations.
+		const userInformations: Partial<UserInformations> =
+			await this.extractUserInformations(data).catch((e) => e);
+		if (userInformations instanceof Error) throw userInformations;
+		// UserData.
+		const userData: Partial<UserData> = await this.extractUserData(
+			data,
+		).catch((e) => e);
+		if (userData instanceof Error) throw userData;
 
-	// Custom edit for field.
-	if (
-		updateObj.password &&
-		(await user.comparePassword(updateObj.password))
-	) {
-		throw new Error(ResponseText.OldPasswordSameNew);
-	}
+		// Group validated fields to update.
+		updateObj = { ...userInformations, ...userData };
 
-	// Group extra data to update.
-	updateObj = { ...updateObj, ...extraData };
-
-	for (let key in updateObj) {
-		if (Object.hasOwn(user._doc, key)) {
-			(user as any)[key] = updateObj[key];
-		} else {
-			delete updateObj[key];
+		// Custom edit for field.
+		if (
+			updateObj.password &&
+			(await user.comparePassword(updateObj.password))
+		) {
+			throw new Error(ResponseText.OldPasswordSameNew);
 		}
-	}
 
-	return user.save();
-};
+		// Group extra data to update.
+		updateObj = { ...updateObj, ...extraData };
+
+		for (let key in updateObj) {
+			if (Object.hasOwn(user._doc, key)) {
+				(user as any)[key] = updateObj[key];
+			} else {
+				delete updateObj[key];
+			}
+		}
+
+		return user.save();
+	},
+);
 
 // middleware.
-UserSchema.pre('save', async function (next) {
+UserSchema.pre('save', async function (next): Promise<void> {
 	const hashPass = async () => {
 		this.password = await Password_Hash(this.password);
 	};
@@ -253,7 +273,7 @@ UserSchema.pre('save', async function (next) {
 
 	next();
 });
-UserSchema.pre('validate', function (next) {
+UserSchema.pre('validate', function (next): void {
 	const passwordLower = this.password.toLowerCase();
 	const emailLower = this.email.toLowerCase();
 	if (
