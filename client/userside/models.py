529e5e47e0820ee51609cac325f072dc32363be7
Lakeserl <dinhlam18112003@gmail.com>
2023-11-29 10:30:39 +0700

    newFile

--- client/userside/models.py
+++ client/userside/models.py
@@ -133,4 +133,13 @@ class Order_details(models.Model):
     
     def __str__(self):
         return self.mname
+class classMembership(models.Model):
+    name = models.CharField(max_length= 100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class User_Profile(models.Model):
+    user = models.OneToOneField(User_Profile, on_delete=models.CASCADE)
+    Membership_plan = models.ForeignKey(Membership_plan, on_delete=models.SET_NULL, null=True)
+)
 
